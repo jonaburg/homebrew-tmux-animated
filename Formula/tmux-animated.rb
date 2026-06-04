9faf2f63dcf394de5b545b0b4894f641e49987ee
jon burga <jon@magnabay.com>
2026-06-04 23:53:30 +0200

    add tmux-animated formula for homebrew

---
+++ Formula/tmux-animated.rb
@@ -0,0 +1,33 @@
+class TmuxAnimated < Formula
+  desc "tmux with smooth animations for window switches and pane operations"
+  homepage "https://github.com/jonaburg/tmux-animated"
+  url "https://github.com/jonaburg/tmux-animated.git",
+      branch: "animations"
+  version "next-3.7.0"
+  license "ISC"
+
+  head "https://github.com/jonaburg/tmux-animated.git", branch: "animations"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libevent"
+  depends_on "ncurses"
+  depends_on "utf8proc"
+
+  def install
+    system "./autogen.sh"
+    system "./configure",
+           "--disable-debug",
+           "--disable-silent-rules",
+           "--prefix=#{prefix}",
+           "--sysconfdir=#{etc}",
+           "--program-suffix=-animated"
+    system "make", "install"
+  end
+
+  test do
+    output = shell_output("#{bin}/tmux-animated -V")
+    assert_match "tmux", output
+  end
+end
